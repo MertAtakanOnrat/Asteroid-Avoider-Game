cf858bb05a60e157c7b830aecf9fa3fd43ea50ec
mertoatakan <mertoatakan@gmail.com>
2023-02-01 17:28:12 +0300

    spaceship is fixed on the phone/tablet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,9 +23,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessInput();
+
+        KeepPlayerOnScreen();
+    }
+
+    void FixedUpdate()
+    {
+        if (movementDirection == Vector3.zero)
+        {
+            return;
+        }
+        rb.AddForce(movementDirection * forceMagnitude * Time.deltaTime, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+    }
+
+    private void ProcessInput()
+    {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
-            Vector2 touchPosition =  Touchscreen.current.primaryTouch.position.ReadValue();
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
 
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
@@ -42,14 +60,31 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private void KeepPlayerOnScreen()
     {
-        if (movementDirection == Vector3.zero)
-        {
-            return;
-        }
-        rb.AddForce(movementDirection * forceMagnitude * Time.deltaTime, ForceMode.Force);
+        Vector3 newPosition = transform.position;
 
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        Vector3 viewportPosition =  mainCamera.WorldToViewportPoint(transform.position);
+
+        if (viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if(viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+
+        if (viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if (viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.x - 0.1f;
+        }
+
+        transform.position = newPosition;
     }
+
 }
